1c9572cd95c3085ba2f96feb1f071e2d8d8424c2
florin <florin.gherasim.dev@gmail.com>
2024-05-18 12:29:55 +0300

    Task 6 - Add option to hide items that have status IsDone - add new class on component keeping item in case isDone - add new js file details.js - add checkbox to toggle show or hide IsDone items

---
+++ Todo/wwwroot/js/details.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#isDoneToggle').change(function () {
+        var display = 'block';
+        var hide = 'none';
+        var displyOption = $(this).prop('checked') == true ? hide : display;
+        $('.onToggleIsDoneHide').each(function () {
+            this.style.display = displyOption;
+        });
+    });
+})
