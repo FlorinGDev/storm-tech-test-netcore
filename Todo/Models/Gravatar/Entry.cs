5f7a0fe85d98f353ac4d511c4d814648579fed87
florin <florin.gherasim.dev@gmail.com>
2024-05-20 14:56:24 +0300

    Task 9 - Add name from gravatar - create asynk call to gravatar and desirealize content received to get name

---
+++ Todo/Models/Gravatar/Entry.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Todo.Models.Gravatar
+{
+    public class Entry
+    {
+        public string DisplayName { get; set; }
+
+    }
+
+    public class GravatarInfo
+    {
+        public List<Entry> Entry { get; set; }
+    }
+}
--- Todo/Services/Gravatar.cs
+++ Todo/Services/Gravatar.cs
@@ -1,5 +1,10 @@
-﻿using System.Security.Cryptography;
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
+using Todo.Models.Gravatar;
 
 namespace Todo.Services
 {
@@ -20,5 +25,28 @@ namespace Todo.Services
                 return builder.ToString().ToLowerInvariant();
             }
         }
+
+        public static async Task<string> GetGravatarUseName(string emailAddress)
+        {
+            string baseAddress = "https://gravatar.com";
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new System.Uri(baseAddress);
+            var response = await httpClient.GetAsync($"/{GetHash(emailAddress)}.json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responsecontent = await response.Content.ReadAsStringAsync();
+                var userNameOnGravatar = JsonConvert.DeserializeObject<GravatarInfo>(responsecontent).Entry.FirstOrDefault()?.DisplayName;
+                return string.IsNullOrWhiteSpace(userNameOnGravatar) ? string.Empty : userNameOnGravatar;
+
+            }
+            else
+            {
+                return string.Empty;
+            }
+
+        }
     }
+
 }
